43d0ed8f2a7424b991e6a6954e465e712f51f4e2
gschiavodev <154530758+gschiavodev@users.noreply.github.com>
2024-02-20 08:03:10 -0500

    Added party guest class for problem 1

--- include/MinotaurBirthdayParty.h
+++ include/MinotaurBirthdayParty.h
@@ -8,3 +8,24 @@ public:
 	static int main();
 
 };
+
+class PartyGuest
+{
+
+private:
+
+	static size_t id;
+	bool part_is_over;
+	bool ate_cupcake;
+	bool is_the_counter;
+	size_t count;
+
+public:
+
+	PartyGuest(size_t id);
+	PartyGuest(size_t id, bool is_counter);
+
+	void count_party_guest();
+	void have_fun_at_the_party();
+
+};
--- src/MinotaurBirthdayParty.cpp
+++ src/MinotaurBirthdayParty.cpp
@@ -1,4 +1,8 @@
 
+
+// System includes
+#include <iostream>
+
 // Local includes
 #include "MinotaurBirthdayParty.h"
 
@@ -10,5 +14,49 @@ int MinotaurBirthdayParty::main()
 
 	// ..
 	return 0;
+
+}
+
+PartyGuest::PartyGuest(size_t id)
+{
+
+	PartyGuest(id, false);
+
+}
+
+PartyGuest::PartyGuest(size_t id, bool is_counter)
+{
+
+	this->id = id;
+	this->part_is_over = false;
+	this->ate_cupcake = false;
+	this->is_the_counter = is_counter;
+	this->count = 0;
+
+	have_fun_at_the_party();
+
+}
+
+void PartyGuest::count_party_guest()
+{
+
+	if (is_the_counter)
+		count++;
+	else
+		std::cerr << "Guest is an imposter!" << std::endl;
+
+}
+
+
+void PartyGuest::have_fun_at_the_party()
+{
+
+	while (!part_is_over) 
+	{
+
+		// ..
+
+
+	}
 
 }
